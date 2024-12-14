c6bef204ce1278f257d9ab38307c67dfbafd53ab
dr-winner <duvorrichardwinner@gmail.com>
2024-12-14 13:15:46 +0000

    Add a test, SeedToken.js file

--- contracts/SeedToken.sol
+++ contracts/SeedToken.sol
@@ -1,13 +1,14 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
- 
+
 error Unauthorized();
- 
+
 contract SeedToken is ERC20 {
   address public owner;
- 
+
   modifier isOwner() {
     if (msg.sender == owner) {
       _;
@@ -15,7 +16,7 @@ contract SeedToken is ERC20 {
       revert Unauthorized();
     }
   }
- 
+
   constructor(
     address ownerAddress,
     string memory name,
@@ -23,11 +24,11 @@ contract SeedToken is ERC20 {
   ) ERC20(name, symbol) {
     owner = ownerAddress;
   }
- 
+
   function changeOwner(address newOwner) external isOwner {
     owner = newOwner;
   }
- 
+
   function mint(uint256 amountOfTokens) external isOwner {
     _mint(msg.sender, amountOfTokens * 10**uint(decimals()));
   }
